9054acc71fb230e92538aea0afcc2c053d236f18
Seogba <tjflwmaks0403@gmail.com>
2022-07-22 00:44:34 +0900

    실패작(api 404)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState , useEffect} from "react";
+
 
 function App() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'dc8ae74dd7msh32aeb2cb02e8137p100e4djsn3f626db9ac6a',
+      'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+    }
+  };
+
+  const[loading,setLoading] = useState(true);
+  const[soccer,setSoccer] = useState([]);
+  const getSoccer = async() =>{
+    const json = await(
+      await fetch(
+        `https://api-football-v1.p.rapidapi.com/v3/timezone`
+      )
+    ).json();
+    setSoccer(json.data.soccer);
+    setLoading(false);
+  };
+
+  useEffect(() =>{
+    getSoccer();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading? <h1>loading,,,</h1> : <div>Hi</div>}
     </div>
-  );
+  )
+
 }
 
 export default App;
+
+
