1e89a716333abc01c93ecb0e48eedd6520a109c1
Seogba <tjflwmaks0403@gmail.com>
2022-08-05 09:50:06 +0900

    api 일일 할당량 초과

--- src/Components/ranking.js
+++ src/Components/ranking.js
@@ -6,27 +6,42 @@ import apikey from "../Data/config";
 const Ranking = () => {
   const [rankings, setRankings] = useState([]);
   const [selectYear, setSelectYear] = useState("2021");
-  const [selectLeague, setSelectLeague] = useState("Euro Championship");
+  const [selectLeague, setSelectLeague] = useState("61");
+  const [loading, setLoading] = useState(true);
 
-  /*useEffect(() => {
-    axios("https://v3.football.api-sports.io/${selectLeague}standings", {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "v3.football.api-sports.io",
-        "x-rapidapi-key": apikey,
-      },
-    })
+  useEffect(() => {
+    axios(
+      `https://v3.football.api-sports.io/standings?league=${selectLeague}&season=${selectYear}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "v3.football.api-sports.io",
+          "x-rapidapi-key": apikey,
+        },
+      }
+    )
       .then((res) => {
-        console.log("res,data:", res);
-        // setRankings(res);
+        console.log("res,data:", res.data.response);
+        setRankings(res.data.response);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);*/
+  }, [selectLeague, selectYear]);
+
+  /*<div className="ranking-show">
+  {loading ? (
+    <h1>now loading,,,</h1>
+  ) : (
+    rankings.map((data) => (
+     
+    ))
+  )}
+</div>*/
 
   return (
-    <div className="ranking-tab">
+    <div className="ranking">
       <div className="select-tab">
         <select
           name="select-league"
@@ -34,10 +49,22 @@ const Ranking = () => {
           defaultValue={selectLeague}
           onChange={(e) => setSelectLeague(e.target.value)}
         >
+          <option value="france">Ligue 1</option>
           <option value="Euro">Euro Championship</option>
           <option value="Confe">Confederations Cup</option>
         </select>
+        <select
+          name="select-year"
+          id="select-year"
+          defaultValue={selectYear}
+          onChange={(e) => setSelectYear(e.target.value)}
+        >
+          <option value="2021">2021</option>
+          <option value="2021">2020</option>
+          <option value="2021">2019</option>
+        </select>
       </div>
+      {console.log(rankings.length)}
     </div>
   );
 };
